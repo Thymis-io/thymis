940b0802a6033769da6681a4bde05bdea6c21fac
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-10-18 19:39:21 +0200

    feat: build modules list from file

--- frontend/src/lib/ModuleCard.svelte
+++ frontend/src/lib/ModuleCard.svelte
@@ -1,3 +1,12 @@
-<div class="card p-4">
-	<img alt="icon" />
+<script lang="ts">
+	export let name: string;
+	export let shortDescription: string;
+	export let icon: string;
+</script>
+
+<div class="card p-4 flex flex-col items-center">
+	<img class="h-32 w-32" src={icon} alt="icon" />
+	<span class="mt-4 font-bold">{name}</span>
+	<span class="mt-4 w-48 flex-1">{shortDescription}</span>
+	<button class="btn mt-8 variant-filled" type="button">Install</button>
 </div>
--- frontend/src/lib/Modules.svelte
+++ frontend/src/lib/Modules.svelte
@@ -1,27 +1,16 @@
 <script>
 	import ModuleCard from './ModuleCard.svelte';
+	import { modules } from './modules';
 	let searchInput = '';
 </script>
 
 <div>
-	<div class="columns-3 mb-16">
-		<input bind:value={searchInput} class="input" type="text" />
+	<div class="columns-3 mb-10">
+		<input bind:value={searchInput} class="input" type="text" placeholder="Search..." />
 	</div>
-	<div
-		class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
-	>
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
-		<ModuleCard />
+	<div class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto py-10">
+		{#each modules as module}
+			<ModuleCard {...module} />
+		{/each}
 	</div>
 </div>
