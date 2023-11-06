d2f7075651abbc5aeaa03e81160f0e6225c642cf
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-11-06 14:43:07 +0100

    feat: save state to json at first init

--- controller/app/dependencies.py
+++ controller/app/dependencies.py
@@ -1,14 +1,24 @@
 import json
+from typing import List
+import pydantic
 from pathlib import Path
-
-from . import models
+from app import models
 
 
 def get_state():
     if not Path("./state.json").is_file():
-        return [
+        init_state = [
             models.Module(name="M1"),
         ]
 
+        with open("./state.json", "w+") as f:
+            json.dump([module.model_dump() for module in init_state], f)
+
     with open("./state.json", "r") as f:
-        return json.load(f)
+        raw_state = json.load(f)
+
+    state = []
+    for entry in raw_state:
+        state.append(models.Module(**entry))
+    
+    return state
