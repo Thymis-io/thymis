f9c614b952c89e28d32e2f83457eb39ca0c64079
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-01-28 14:49:58 +0100

    feat: added FastAPI stub routes for build and deploy

--- controller/app/routes/router.py
+++ controller/app/routes/router.py
@@ -1,6 +1,6 @@
 from typing import List
 from app.models.state import State
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, BackgroundTasks
 from ..dependencies import get_or_init_state
 from app import models
 from app.crud import state
@@ -22,3 +22,18 @@ def get_available_modules(state: State = Depends(get_or_init_state)):
 @router.patch("/state")
 async def update_state(new_state: Request):
     return state.update(await new_state.json())
+
+
+@router.get("/build_status")
+def get_build_status():
+    return
+
+
+@router.post("/action/build")
+def build_nix(background_tasks: BackgroundTasks):
+    return
+
+
+@router.post("/action/deploy")
+def deploy(background_tasks: BackgroundTasks):
+    return
