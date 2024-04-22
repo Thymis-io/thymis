5634815080e07a04a7858ac6244e0c183c103d89
Eli Kogan-Wang <elikowa@gmail.com>
2024-04-22 17:01:51 +0200

    Update controllerHost in api.ts to use current host in browser environment

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,12 @@
+import { browser } from '$app/environment';
 import { env } from '$env/dynamic/public';
 
-export const controllerHost = env.PUBLIC_CONTROLLER_HOST || '127.0.0.1:8000';
+export let controllerHost = env.PUBLIC_CONTROLLER_HOST || '127.0.0.1:8000';
+if (browser) {
+	// set controllerHost to the current host:8000
+	controllerHost = window.location.hostname + ':8000';
+}
+console.log('controllerHost:', controllerHost);
 export const controllerProtocol = env.PUBLIC_CONTROLLER_PROTOCOL || 'http';
 
 if (!env.PUBLIC_CONTROLLER_HOST) {
