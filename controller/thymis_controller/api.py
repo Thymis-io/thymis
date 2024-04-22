5b9957ad91f251fe841c77442e20deb0f12302ff
Eli Kogan-Wang <elikowa@gmail.com>
2024-04-22 17:31:00 +0200

    Fix CORS middleware configuration in api.py

--- controller/thymis_controller/api.py
+++ controller/thymis_controller/api.py
@@ -35,7 +35,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
