93f702b6599cef7b7d799635c4f0cf01335a4f48
Eli Kogan-Wang <elikowa@gmail.com>
2024-05-30 00:04:14 +0200

    Correctly place pylint annotation on models/__init__.py

--- controller/thymis_controller/models/__init__.py
+++ controller/thymis_controller/models/__init__.py
@@ -3,7 +3,9 @@ from .state import *
 from .task import *
 
 __all__ = (
-    module.__all__ + state.__all__ + task.__all__
-)  # pylint: disable=undefined-variable,no-member
+    module.__all__  # pylint: disable=undefined-variable,no-member
+    + state.__all__  # pylint: disable=undefined-variable
+    + task.__all__  # pylint: disable=undefined-variable
+)
 
 # See https://stackoverflow.com/questions/60440945/correct-way-to-re-export-modules-from-init-py
