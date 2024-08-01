57354f55f531efa40f83cbb415c4599cce31ba66
Maximilian Schmoecker <m.schmoecker@brickfire.eu>
2024-08-01 19:11:29 +0200

    feat: added noVNC viewer

--- frontend/src/routes/vnc/+page.svelte
+++ frontend/src/routes/vnc/+page.svelte
@@ -2,7 +2,31 @@
 	import type { PageData } from './$types';
 	import { P, Card } from 'flowbite-svelte';
 	import DeployActions from '$lib/components/DeployActions.svelte';
-	import { state } from '$lib/state';
+	import { state, type Device } from '$lib/state';
+	import { browser } from '$app/environment';
+	import { controllerHost } from '$lib/api';
+
+	const initVNC = async (device: Device) => {
+		// @ts-ignore
+		let RFB = await import('@novnc/novnc/lib/rfb.js');
+
+		const port = '5901';
+		const url = `ws://${device.targetHost}:${port}/websockify`;
+
+		const password = '';
+		const canvas = document.getElementById(`vnc-canvas-${device.targetHost}`);
+		const rfb = new RFB.default(canvas, url, { credentials: { password: password } });
+
+		rfb.viewOnly = true;
+		rfb.scaleViewport = true;
+		rfb.showDotCursor = true;
+	};
+
+	if (browser) {
+		for (const device of $state.devices) {
+			initVNC(device);
+		}
+	}
 
 	export let data: PageData;
 </script>
@@ -17,7 +41,7 @@
 			<P class="mb-2 text-center">{device.displayName}</P>
 			<Card class="w-full max-w-none">
 				<pre>vncviewer {device.targetHost}:5900</pre>
-				<img alt=" " class="w-full aspect-video" />
+				<div id={`vnc-canvas-${device.targetHost}`} class="w-full aspect-video mt-4" />
 			</Card>
 		</div>
 	{/each}
