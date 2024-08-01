4d511c7c8d403cc1d4b94caec49eb056b4b34490
Eli Kogan-Wang <elikowa@gmail.com>
2024-08-01 17:59:40 +0200

    fix top bar stickyness by reverting app.html to upstream sveltekit and adding a div in the layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -32,14 +32,16 @@
 	let drawerHidden = true;
 </script>
 
-<header
-	class="sticky top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800"
->
-	<Navbar bind:drawerHidden />
-</header>
-<div class="overflow-hidden lg:flex">
-	<Sidebar bind:drawerHidden />
-	<div class="relative h-full w-full overflow-y-auto lg:ml-64 p-4">
-		<slot />
+<div class="bg-gray-50 dark:bg-gray-900">
+	<header
+		class="sticky top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800"
+	>
+		<Navbar bind:drawerHidden />
+	</header>
+	<div class="overflow-hidden lg:flex">
+		<Sidebar bind:drawerHidden />
+		<div class="relative h-full w-full overflow-y-auto lg:ml-64 p-4">
+			<slot />
+		</div>
 	</div>
 </div>
