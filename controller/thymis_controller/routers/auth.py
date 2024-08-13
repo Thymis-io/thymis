ef1df9f820cbaa5ddb6c605ed3b0eedc94143c58
Eli Kogan-Wang <elikowa@gmail.com>
2024-08-13 16:32:58 +0200

    enable basic authentication in login route

--- controller/thymis_controller/routers/auth.py
+++ controller/thymis_controller/routers/auth.py
@@ -1,17 +1,34 @@
 import datetime
-from typing import Annotated, Dict
 import uuid
-from fastapi import APIRouter, FastAPI, Depends, HTTPException, Request, Response, status
+from typing import Annotated, Dict
+
+import httpx
+from fastapi import (
+    APIRouter,
+    Depends,
+    FastAPI,
+    HTTPException,
+    Request,
+    Response,
+    status,
+)
 from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-import httpx
 from pydantic import BaseModel
 from thymis_controller.config import global_settings
-from thymis_controller.dependencies import SessionTicket, get_session, require_valid_session, apply_session, invalidate_session
+from thymis_controller.dependencies import (
+    SessionTicket,
+    apply_session,
+    get_session,
+    invalidate_session,
+    require_valid_session,
+)
+
 
 class AuthMethods(BaseModel):
     basic: bool
     oauth2: bool
+
 
 REDIRECT_URI = global_settings.BASE_URL + "/auth/callback"
 
@@ -20,20 +37,34 @@ router = APIRouter(
 )
 basicAuth = HTTPBasic()
 
+
 # only enable basic auth if the flag is set
 @router.post("/login/basic")
-def login_basic(credentials: Annotated[HTTPBasicCredentials, Depends(basicAuth)], response: Response):
-    if not global_settings.AUTH_BASIC_ENABLED:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Basic auth is disabled")
-    if credentials.username == global_settings.AUTH_BASIC_USERNAME and credentials.password == global_settings.AUTH_BASIC_PASSWORD: # TODO replace password check with hash comparison
+def login_basic(
+    credentials: Annotated[HTTPBasicCredentials, Depends(basicAuth)], response: Response
+):
+    if not global_settings.AUTH_BASIC:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Basic auth is disabled"
+        )
+    if (
+        credentials.username == global_settings.AUTH_BASIC_USERNAME
+        and credentials.password == global_settings.AUTH_BASIC_PASSWORD
+    ):  # TODO replace password check with hash comparison
         apply_session(response, None)
         return {"message": "Logged in"}
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+        )
+
 
 @router.get("/auth/methods", response_model=AuthMethods)
 def get_auth_methods():
-    return AuthMethods(basic=global_settings.AUTH_BASIC, oauth2=global_settings.AUTH_OAUTH)
+    return AuthMethods(
+        basic=global_settings.AUTH_BASIC, oauth2=global_settings.AUTH_OAUTH
+    )
+
 
 # Route to redirect user to the OAuth2 provider's authorization URL
 @router.get("/login/oauth2")
@@ -42,10 +73,15 @@ def login():
         f"{global_settings.AUTH_OAUTH_AUTHORIZATION_ENDPOINT}?response_type=code&client_id={global_settings.AUTH_OAUTH_CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=openid profile email"
     )
 
+
 @router.post("/logout")
-def logout(response: Response, session: Annotated[SessionTicket, Depends(require_valid_session)]):
+def logout(
+    response: Response,
+    session: Annotated[SessionTicket, Depends(require_valid_session)],
+):
     invalidate_session(response)
     return {"message": "Logged out"}
+
 
 # Route to handle the OAuth2 provider's callback
 @router.get("/callback")
@@ -63,12 +99,17 @@ async def callback(code: str, response: Response):
         )
 
     if token_response.status_code != 200:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Token exchange failed")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Token exchange failed"
+        )
 
     token_data = token_response.json()
     apply_session(response, token_data["access_token"])
-    return RedirectResponse("/", headers=response.headers) # necessary to set the cookies
+    return RedirectResponse(
+        "/", headers=response.headers
+    )  # necessary to set the cookies
 
-@router.get("/protected") # TODO remove debug route
+
+@router.get("/protected")  # TODO remove debug route
 def read_protected(token: Annotated[Dict, Depends(require_valid_session)]):
     return {"message": "Hello World"}
