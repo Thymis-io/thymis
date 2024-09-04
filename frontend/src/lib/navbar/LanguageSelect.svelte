e81dd943e06aeb6620709aa0a437aff3371cf42a
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-09-04 15:40:33 +0200

    fix: responsive navbar

--- frontend/src/lib/navbar/LanguageSelect.svelte
+++ frontend/src/lib/navbar/LanguageSelect.svelte
@@ -9,6 +9,11 @@
 		{ name: $t('language.de'), value: 'de' }
 	];
 
+	let localesShort = [
+		{ name: 'en', value: 'en' },
+		{ name: 'de', value: 'de' }
+	];
+
 	onMount(() => {
 		if (browser) {
 			let cookie = document.cookie.split(';').find((c) => c.trim().startsWith('locale='));
@@ -26,9 +31,17 @@
 </script>
 
 <Select
-	class="ml-2 w-32"
+	class="ml-2 w-32 hidden md:block"
 	size="sm"
 	items={locales}
+	bind:value={$locale}
+	placeholder={$t('language.select')}
+/>
+
+<Select
+	class="ml-1 w-16 block md:hidden"
+	size="sm"
+	items={localesShort}
 	bind:value={$locale}
 	placeholder={$t('language.select')}
 />
--- frontend/src/lib/navbar/Navbar.svelte
+++ frontend/src/lib/navbar/Navbar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { t } from 'svelte-i18n';
-	import UserMenu from './UserMenu.svelte';
 	import { DarkMode, NavBrand, NavHamburger, Navbar, Search, ToolbarButton } from 'flowbite-svelte';
 	import GithubSolid from 'flowbite-svelte-icons/GithubSolid.svelte';
 	import LanguageSelect from './LanguageSelect.svelte';
@@ -16,23 +15,22 @@
 	<div class="flex w-fit items-center justify-start">
 		<NavHamburger
 			onClick={() => (drawerHidden = !drawerHidden)}
-			class="m-0 me-3 md:block lg:hidden"
+			class="m-0 me-2 md:block lg:hidden"
 		/>
-		<NavBrand href="/" class="ms-2 md:me-24">
+		<NavBrand href="/" class="ms-2 me-2 sm:me-8 lg:me-24">
 			<img src="/favicon.png" class="me-3 h-6 sm:h-8" alt="Thymis Logo" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl">
+			<span
+				class="self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl hidden sm:block"
+			>
 				Thymis
 			</span>
 		</NavBrand>
-		<div class="w-8 lg:hidden">
-			<Search size="md" placeholder={$t('common.search')} />
-		</div>
-		<div class="w-96 hidden lg:block">
+		<div class="ms-2 w-32 sm:w-48 lg:w-64 xl:w-96 block">
 			<Search size="md" placeholder={$t('common.search')} />
 		</div>
 	</div>
 
-	<div class="ms-auto flex items-center gap-2 p-1">
+	<div class="ms-auto flex items-center sm:gap-2 p-1">
 		<a
 			class="github-button"
 			href="https://github.com/thymis-io/thymis"
@@ -42,7 +40,6 @@
 				<GithubSolid />
 			</ToolbarButton>
 		</a>
-		<UserMenu />
 		<DarkMode />
 		<LanguageSelect />
 	</div>
