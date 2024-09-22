52b08d9e3b37bef3eda88f0f5f8a262c76ea243c
Florian Schönlau <f.schoenlau@brickfire.eu>
2024-09-22 15:18:53 +0200

    feat: add device details page to sidebar navigation

---
+++ frontend/src/routes/(authenticated)/device-details/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { P } from 'flowbite-svelte';
+	import {
+		globalNavSelectedDevice,
+		globalNavSelectedTag,
+		globalNavSelectedTargetType,
+		state
+	} from '$lib/state';
+	import DeployActions from '$lib/components/DeployActions.svelte';
+
+	export let data: PageData;
+</script>
+
+<div class="flex justify-between mb-4">
+	<div />
+	<DeployActions />
+</div>
+{#if $globalNavSelectedTargetType === 'device' && $globalNavSelectedDevice}
+	<p>Test</p>
+{:else if $globalNavSelectedTargetType === 'tag' && $globalNavSelectedTag}
+	<div class="grid grid-cols-3 gap-4">
+		{#each data.state.devices.filter( (device) => device.tags.includes($globalNavSelectedTag.identifier) ) as device}
+			<p>Test</p>
+		{/each}
+	</div>
+{/if}
