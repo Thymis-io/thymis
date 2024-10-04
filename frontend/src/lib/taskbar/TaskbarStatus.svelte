5c2ff14ebff2fceb91416a021115a3fd9eb8e62f
Eli Kogan-Wang <elikowa@gmail.com>
2024-10-04 16:23:56 +0200

    refactor: Improve progress bar scaling function in TaskbarStatus component

--- frontend/src/lib/taskbar/TaskbarStatus.svelte
+++ frontend/src/lib/taskbar/TaskbarStatus.svelte
@@ -8,13 +8,23 @@
 	// progress bar in task.data.nix_process.global_done and global_expected
 
 	let progress = 0;
+	const progressScalingFunction = (done: number, expected: number) => {
+		const f = (x: number) => x * x + 1;
+		// if both are 0, progress is 0
+		if (done === 0) {
+			return 0;
+		} else {
+			return (f(done) / f(expected)) * 100;
+		}
+	};
 	$: if (isNixTask(task)) {
 		// if both are 0, progress is 0
-		if (task.status.done === 0) {
-			progress = 0;
-		} else {
-			progress = (task.status.done / task.status.expected) * 100;
-		}
+		// if (task.status.done === 0) {
+		// 	progress = 0;
+		// } else {
+		// 	progress = (task.status.done / task.status.expected) * 100;
+		// }
+		progress = progressScalingFunction(task.status.done, task.status.expected);
 	}
 </script>
 
