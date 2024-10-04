66685733a683a240bdbee39d426ab5884ab0a5d9
Eli Kogan-Wang <elikowa@gmail.com>
2024-10-04 15:00:25 +0200

    Add page titles

--- frontend/src/routes/(authenticated)/+layout.svelte
+++ frontend/src/routes/(authenticated)/+layout.svelte
@@ -49,6 +49,10 @@
 	let drawerHidden = true;
 </script>
 
+<svelte:head>
+	<title>Thymis Dashboard</title>
+</svelte:head>
+
 <div class="contents bg-gray-50 dark:bg-gray-900 dark:text-white">
 	<header
 		class="fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800"
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -8,6 +8,10 @@
 	const authError: Writable<string | null> = queryParam('authError');
 </script>
 
+<svelte:head>
+	<title>Thymis - Sign in</title>
+</svelte:head>
+
 <section class="bg-gray-50 dark:bg-gray-900">
 	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 		<div
