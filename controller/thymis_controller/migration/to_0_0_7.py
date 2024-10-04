7fd194eefa23766444a7bca85e2d5df1be2c93c4
Eli Kogan-Wang <elikowa@gmail.com>
2024-10-04 14:14:58 +0200

    log targethost field removal

--- controller/thymis_controller/migration/to_0_0_7.py
+++ controller/thymis_controller/migration/to_0_0_7.py
@@ -3,6 +3,9 @@ def to_0_0_7(state: dict):
 
     def remove_taget_host(device_settings):
         if "targetHost" in device_settings:
+            print(
+                f"Removing targetHost {device_settings['targetHost']} from device {device_settings['displayName']} ({device_settings['identifier']})"
+            )
             del device_settings["targetHost"]
 
     for device in state["devices"]:
