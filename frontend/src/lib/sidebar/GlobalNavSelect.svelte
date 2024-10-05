c743b262596429316fcf7c8d357e09cb701a6421
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-10-05 16:56:33 +0200

    fix: nav select glitch

--- frontend/src/lib/sidebar/GlobalNavSelect.svelte
+++ frontend/src/lib/sidebar/GlobalNavSelect.svelte
@@ -34,7 +34,11 @@
 	</div>
 	<ChevronDownOutline class="h-4 ms-2 text-white dark:text-white" /></Button
 >
-<Dropdown class="overflow-y-auto px-3 pb-3 text-sm max-h-96">
+<Dropdown
+	class="overflow-y-auto px-3 pb-3 text-sm h-full relative"
+	containerClass="z-50 left-4 right-4 flex flex-col whitespace-nowrap"
+	strategy="absolute"
+>
 	<div slot="header" class="p-3">
 		<Search size="md" bind:value={search} placeholder={$t('common.search')} />
 	</div>
@@ -44,7 +48,7 @@
 				href={`?${buildGlobalNavSearchParam($page.url.search, 'tag', tag.identifier)}`}
 				class={'flex gap-2 my-1 p-1 hover:bg-primary-500 items-center rounded'}
 			>
-				<TagIcon size={22} />
+				<TagIcon size={22} class="shrink-0" />
 				<span class="text-base">{tag.displayName}</span>
 			</DropdownItem>
 		{/if}
@@ -55,7 +59,7 @@
 				href={`?${buildGlobalNavSearchParam($page.url.search, 'device', device.identifier)}`}
 				class={'flex gap-2 my-1 p-1 hover:bg-primary-500 items-center rounded'}
 			>
-				<HardDrive size={22} />
+				<HardDrive size={22} class="shrink-0" />
 				<span class="text-base">{device.displayName}</span>
 			</DropdownItem>
 		{/if}
