ce9c8acdae78555932265812d94165ef5276b2af
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-10-12 19:11:42 +0200

    feat: basic git info

--- frontend/src/routes/(authenticated)/history/+page.ts
+++ frontend/src/routes/(authenticated)/history/+page.ts
@@ -1,14 +1,31 @@
-import type { Commit } from '$lib/history';
+import type { Commit, GitInfo } from '$lib/history';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	const response = await fetch(`/api/history`, {
+	const history_response = fetch(`/api/history`, {
+		method: 'GET',
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+	const git_info_response = fetch(`/api/git/info`, {
 		method: 'GET',
 		headers: {
 			'content-type': 'application/json'
 		}
 	});
 	return {
-		history: response.json() as Promise<Commit[]>
+		history: new Promise((resolve, reject) => {
+			history_response
+				.then((res) => res.json())
+				.then((json) => resolve(json as Commit[]))
+				.catch(reject);
+		}) as Promise<Commit[]>,
+		gitInfo: new Promise((resolve, reject) => {
+			git_info_response
+				.then((res) => res.json())
+				.then((json) => resolve(json as GitInfo))
+				.catch(reject);
+		}) as Promise<GitInfo>
 	};
 }) satisfies PageLoad;
