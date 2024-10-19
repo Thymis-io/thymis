7fd6d321a47be1cd067f9d19fdb3caf0a6aff619
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-10-19 12:41:49 +0200

    fix: websocket connection and queue notifications if not send

--- controller/thymis_controller/notifications.py
+++ controller/thymis_controller/notifications.py
@@ -1,19 +1,27 @@
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import BackgroundTasks, WebSocket, WebSocketDisconnect
 from fastapi.websockets import WebSocketState
 
 
 class NotificationManager:
+    queued_messages: list[str] = []
+
     def __init__(self):
         self.active_connections: list[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
+
+        queued_messages = list(self.queued_messages)
+        self.queued_messages = []
+        for message in queued_messages:
+            await self.broadcast(message)
+
         try:
-            while True:
+            while websocket.application_state != WebSocketState.DISCONNECTED:
                 await websocket.receive_bytes()
         except WebSocketDisconnect:
-            await self.disconnect(websocket)
+            self.disconnect(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
@@ -22,6 +30,7 @@ class NotificationManager:
         await websocket.send_json({"message": message})
 
     async def broadcast(self, message: str):
+        send_anywhere = False
         for connection in self.active_connections:
             if (
                 connection.application_state == WebSocketState.DISCONNECTED
@@ -30,9 +39,12 @@ class NotificationManager:
                 continue
             try:
                 await connection.send_json({"message": message})
+                send_anywhere = True
             except Exception as e:
                 print(e)
                 self.active_connections.remove(connection)
+        if not send_anywhere:
+            self.queued_messages.append(message)
 
 
 notification_manager = NotificationManager()
