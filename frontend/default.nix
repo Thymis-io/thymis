7356fa0ff97e9bd8f5f98286c4b19dd84358b2cb
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-10-19 12:44:50 +0200

    fix: npmDepsHash

--- frontend/default.nix
+++ frontend/default.nix
@@ -7,7 +7,7 @@ buildNpmPackage {
   pname = "thymis-frontend";
   version = "0.0.1";
   src = ./.;
-  npmDepsHash = "sha256-FYbyxT/d9MdraeGTCD4WziBbeE530h5Bv+bvcrCHU7A=";
+  npmDepsHash = "sha256-3GNb5/0VcQFez6SX6hcKFloUQ4sRdBNJfsPOHLx3EY4=";
   postInstall = ''
     mkdir -p $packageOut/build
     cp -r ./build/* $packageOut/build
