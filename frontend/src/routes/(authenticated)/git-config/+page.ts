127439afcc0943feb2394491177c7e90993e01f8
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-10-26 17:20:49 +0200

    fix: promise

--- frontend/src/routes/(authenticated)/git-config/+page.ts
+++ frontend/src/routes/(authenticated)/git-config/+page.ts
@@ -2,18 +2,13 @@ import type { GitInfo } from '$lib/history';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	const git_info_response = fetch(`/api/git/info`, {
+	const git_info_response = await fetch(`/api/git/info`, {
 		method: 'GET',
 		headers: {
 			'content-type': 'application/json'
 		}
 	});
 	return {
-		gitInfo: (await new Promise((resolve, reject) => {
-			git_info_response
-				.then((res) => res.json())
-				.then((json) => resolve(json as GitInfo))
-				.catch(reject);
-		})) as GitInfo
+		gitInfo: (await git_info_response.json()) as GitInfo
 	};
 }) satisfies PageLoad;
