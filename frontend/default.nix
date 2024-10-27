f0743faf83ccf9b1fcee8c846d0598039a4b7e4a
Eli Kogan-Wang <elikowa@gmail.com>
2024-10-27 14:06:15 +0100

    update dependencies: controller, frontend, flake

--- frontend/default.nix
+++ frontend/default.nix
@@ -7,7 +7,7 @@ buildNpmPackage {
   pname = "thymis-frontend";
   version = "0.0.1";
   src = ./.;
-  npmDepsHash = "sha256-R/KXGZG9H4Tb0sK+ti+o1hgiTec5turFqf7qv2wKQ7g=";
+  npmDepsHash = "sha256-FYbyxT/d9MdraeGTCD4WziBbeE530h5Bv+bvcrCHU7A=";
   postInstall = ''
     mkdir -p $packageOut/build
     cp -r ./build/* $packageOut/build
