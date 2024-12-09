e1c0296b5b46c5116db06624c4dfa3ec1fd86ae3
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-09 17:56:32 +0100

    wip monday

--- controller/thymis_controller/main.py
+++ controller/thymis_controller/main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 from thymis_controller.config import global_settings
 from thymis_controller.notifications import NotificationManager
 from thymis_controller.routers import agent, api, auth, frontend
-from thymis_controller.task.executor import TaskWorkerPoolManager
+from thymis_controller.task import TaskController
 
 logger = logging.getLogger(__name__)
 
@@ -139,13 +139,17 @@ async def lifespan(app: FastAPI):
     init_ssh_key()
     notification_manager = NotificationManager()
     notification_manager.start()
-    task_worker_pool = TaskWorkerPoolManager()
+    task_controller = TaskController()
+    await task_controller.start()
     logger.info("starting frontend")
     await frontend.frontend.run()
     logger.info("frontend started")
     logger.info("frontend raise_if_terminated task created")
     logger.info("Starting controller at \033[1m%s\033[0m", global_settings.BASE_URL)
-    yield {"notification_manager": notification_manager, "task_worker_pool": task_worker_pool}
+    yield {
+        "notification_manager": notification_manager,
+        "task_controller": task_controller,
+    }
     notification_manager.stop()
     logger.info("stopping frontend")
     await frontend.frontend.stop()
