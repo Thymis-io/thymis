7fdf157aa76290ac1433aa7a57bc3c0f89b06b07
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-16 21:03:46 +0100

    wip monday

--- controller/thymis_controller/task/worker.py
+++ controller/thymis_controller/task/worker.py
@@ -1,4 +1,6 @@
+import subprocess
 from multiprocessing.connection import Connection
+from typing import List
 
 import thymis_controller.models.task as models_task
 
@@ -29,8 +31,27 @@ def reject_task(reason: str, task: models_task.TaskSubmission, conn: Connection)
     )
 
 
-def project_flake_update_task():
+def project_flake_update_task(task: models_task.TaskSubmission, conn: Connection):
     pass
 
 
 SUPPORTED_TASK_TYPES = {"project_flake_update_task": project_flake_update_task}
+
+
+def run_command(
+    task: models_task.TaskSubmission,
+    conn: Connection,
+    args: List[str],
+    env: dict,
+    cwd: str,
+):
+    proc = subprocess.Popen(
+        args,
+        env=env,
+        cwd=cwd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    # thread to read stdout and stderr
