e8550aa7bd075eeadbf7839ea38873f3704a0737
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-16 20:29:40 +0100

    wip monday again

--- controller/thymis_controller/task/worker.py
+++ controller/thymis_controller/task/worker.py
@@ -4,9 +4,11 @@ import thymis_controller.models.task as models_task
 
 
 def worker_run_task(task: models_task.TaskSubmission, conn: Connection):
-    if task.data.type not in []:
+    if task.data.type not in SUPPORTED_TASK_TYPES:
         reject_task(f"Task type {task.data.type} not supported", task, conn)
+        return
     pick_task(task, conn)
+    SUPPORTED_TASK_TYPES[task.data.type](task, conn)
 
 
 def pick_task(task: models_task.TaskSubmission, conn: Connection):
@@ -25,3 +27,10 @@ def reject_task(reason: str, task: models_task.TaskSubmission, conn: Connection)
             id=task.id, update=models_task.TaskRejectedUpdate(reason=reason)
         )
     )
+
+
+def project_flake_update_task():
+    pass
+
+
+SUPPORTED_TASK_TYPES = {"project_flake_update_task": project_flake_update_task}
