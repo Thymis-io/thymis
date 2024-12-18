391c1b7d8b351e04ff3e467667c1cda5a30e7748
Adrian Block <adrian@adrblo.de>
2024-12-18 02:42:08 +0100

    added relations and register flow for deployment_info and hardware_devices

---
+++ controller/thymis_controller/db_models/deployment_info.py
@@ -0,0 +1,27 @@
+import uuid
+from typing import TYPE_CHECKING, List
+
+from sqlalchemy import Uuid
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from thymis_controller.database.base import Base
+
+if TYPE_CHECKING:
+    from thymis_controller.db_models.hardware_device import HardwareDevice
+
+
+class DeploymentInfo(Base):
+    __tablename__ = "deployment_info"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid(as_uuid=True), primary_key=True, index=True
+    )
+
+    ssh_public_key: Mapped[str] = mapped_column(nullable=False, unique=True)
+    deployed_config_commit: Mapped[str]
+    deployed_config_id: Mapped[str]
+
+    reachable_deployed_host: Mapped[str | None]
+
+    hardware_devices: Mapped[List["HardwareDevice"]] = relationship(
+        back_populates="deployment_info"
+    )
