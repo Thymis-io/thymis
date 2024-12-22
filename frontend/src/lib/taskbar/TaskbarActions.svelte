62f2b0ca977170173d46b3448c8eeae6352249b6
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-12-22 13:38:39 +0100

    chore: translate task table

--- frontend/src/lib/taskbar/TaskbarActions.svelte
+++ frontend/src/lib/taskbar/TaskbarActions.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { type Task, cancelTask, retryTask, runImmediately } from '$lib/taskstatus';
+	import { t } from 'svelte-i18n';
+	import { type TaskShort, cancelTask, retryTask, runImmediately } from '$lib/taskstatus';
 	import { page } from '$app/stores';
 	import { Button } from 'flowbite-svelte';
 	export let task: TaskShort;
@@ -7,26 +8,43 @@
 
 <div class="flex flex-row items-center space-x-2">
 	{#if task.state === 'pending'}
-		<Button class="btn btn-sm btn-danger px-4 py-2" on:click={() => cancelTask(task.id)}>
-			Cancel
+		<Button
+			class="btn btn-sm btn-danger px-4 py-2 whitespace-nowrap"
+			on:click={() => cancelTask(task.id)}
+		>
+			{$t('taskbar.cancel')}
 		</Button>
-		<Button class="btn btn-sm btn-primary px-4 py-2" on:click={() => runImmediately(task.id)}>
-			Run Immediately
+		<Button
+			class="btn btn-sm btn-primary px-4 py-2 whitespace-nowrap"
+			on:click={() => runImmediately(task.id)}
+		>
+			{$t('taskbar.run-immediately')}
 		</Button>
 	{:else if task.state === 'running'}
-		<Button class="btn btn-sm btn-danger px-4 py-2" on:click={() => cancelTask(task.id)}>
-			Cancel
+		<Button
+			class="btn btn-sm btn-danger px-4 py-2 whitespace-nowrap"
+			on:click={() => cancelTask(task.id)}
+		>
+			{$t('taskbar.cancel')}
 		</Button>
 	{:else if task.state === 'completed'}
-		<Button class="btn btn-sm btn-primary px-4 py-2" on:click={() => retryTask(task.id)}>
-			Retry
+		<Button
+			class="btn btn-sm btn-primary px-4 py-2 whitespace-nowrap"
+			on:click={() => retryTask(task.id)}
+		>
+			{$t('taskbar.retry')}
 		</Button>
 	{:else if task.state === 'failed'}
-		<Button class="btn btn-sm btn-primary px-4 py-2" on:click={() => retryTask(task.id)}>
-			Retry
+		<Button
+			class="btn btn-sm btn-primary px-4 py-2 whitespace-nowrap"
+			on:click={() => retryTask(task.id)}
+		>
+			{$t('taskbar.retry')}
 		</Button>
 	{/if}
 	<a data-sveltekit-preload-data="tap" href="/tasks/{task.id}{$page.url.search}">
-		<Button class="btn btn-sm btn-primary px-4 py-2">View</Button>
+		<Button class="btn btn-sm btn-primary px-4 py-2 whitespace-nowrap">
+			{$t('taskbar.details')}
+		</Button>
 	</a>
 </div>
