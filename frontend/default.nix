db92cba21649d18acf086edca949aed60de7649a
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-22 14:09:52 +0100

    wip tuesday

--- frontend/default.nix
+++ frontend/default.nix
@@ -7,7 +7,7 @@ buildNpmPackage {
   pname = "thymis-frontend";
   version = "0.0.1";
   src = ./.;
-  npmDepsHash = "sha256-XiC2IGPN0XpngVK73qeFiyp7B1SnZEQDGCHUYTZ5NSo=";
+  npmDepsHash = lib.fakeHash;
   postInstall = ''
     mkdir -p $packageOut/build
     cp -r ./build/* $packageOut/build
---
+++ frontend/src/lib/taskbar/svelte-resizable-columns.d.ts
@@ -0,0 +1,1 @@
+declare module 'svelte-resizable-columns/src/ResizableColumns';
