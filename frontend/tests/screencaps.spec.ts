e17089824bf818ea362e9ef2733006477dff13bd
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-23 13:52:27 +0100

    tests support dark mode

--- frontend/tests/screencaps.spec.ts
+++ frontend/tests/screencaps.spec.ts
@@ -2,85 +2,93 @@ import { test, expect } from '../playwright/fixtures';
 
 test.describe.configure({ mode: 'serial' });
 
-test('overview page shows overview', async ({ page }) => {
-	await page.goto('/login');
+const colorSchemes = ['light', 'dark'] as const;
 
-	await page.fill('input[name="username"]', 'admin');
-	await page.fill('input[name="password"]', 'testadminpassword');
-	await page.click('button[type="submit"]');
+colorSchemes.forEach((colorScheme) => {
+	test.describe(`Color scheme: ${colorScheme}`, () => {
+		test.use({ colorScheme: colorScheme });
 
-	await page.waitForURL('http://localhost:8000/overview');
+		test('overview page shows overview', async ({ page }) => {
+			await page.goto('/login');
 
-	await expect(page).toHaveScreenshot();
-});
+			await page.fill('input[name="username"]', 'admin');
+			await page.fill('input[name="password"]', 'testadminpassword');
+			await page.click('button[type="submit"]');
 
-test('shows devices', async ({ page }) => {
-	const resp = await page.goto('/devices');
+			await page.waitForURL('http://localhost:8000/overview');
 
-	// 200 OK is expected
-	expect(resp?.status()).toBe(200);
-	await expect(page).toHaveScreenshot();
+			await expect(page).toHaveScreenshot();
+		});
 
-	// We can add Devices
-	const addDeviceButton = page.locator('button').filter({ hasText: 'Create New Device' });
-	await addDeviceButton.click();
+		test('shows devices', async ({ page }) => {
+			const resp = await page.goto('/devices');
 
-	await expect(page).toHaveScreenshot();
+			// 200 OK is expected
+			expect(resp?.status()).toBe(200);
+			await expect(page).toHaveScreenshot();
 
-	// select id display-name input and fill it with 'My Device 1'
-	const displayNameInput = page.locator('#display-name').first();
-	await displayNameInput.fill('My Device 1');
+			// We can add Devices
+			const addDeviceButton = page.locator('button').filter({ hasText: 'Create New Device' });
+			await addDeviceButton.click();
 
-	const deviceTypeSelect = page.locator('#device-type').first();
-	await deviceTypeSelect.selectOption({ label: 'Raspberry Pi 4' });
+			await expect(page).toHaveScreenshot();
 
-	const saveButton = page.locator('button').filter({ hasText: 'Create device' });
-	await saveButton.click();
+			// select id display-name input and fill it with 'My Device 1'
+			const displayNameInput = page.locator('#display-name').first();
+			await displayNameInput.fill('My Device 1');
 
-	await expect(page).toHaveScreenshot();
+			const deviceTypeSelect = page.locator('#device-type').first();
+			await deviceTypeSelect.selectOption({ label: 'Raspberry Pi 4' });
 
-	// go to device details page, delete device
-	const viewDetailsButton = page.locator('a', { hasText: 'View Details' }).first();
-	await viewDetailsButton.click();
+			const saveButton = page.locator('button').filter({ hasText: 'Create device' });
+			await saveButton.click();
 
-	await expect(page).toHaveScreenshot();
+			await expect(page).toHaveScreenshot();
 
-	const deleteButton = page.locator('button').filter({ hasText: 'Delete' });
-	await deleteButton.click();
+			// go to device details page, delete device
+			const viewDetailsButton = page.locator('a', { hasText: 'View Details' }).first();
+			await viewDetailsButton.click();
 
-	await expect(page).toHaveScreenshot();
+			await expect(page).toHaveScreenshot();
 
-	const confirmButton = page.locator('button').filter({ hasText: 'Delete' }).nth(0);
-	await confirmButton.click();
+			const deleteButton = page.locator('button').filter({ hasText: 'Delete' });
+			await deleteButton.click();
 
-	await expect(page).toHaveScreenshot();
+			await expect(page).toHaveScreenshot();
 
-	const devicesNav = page.locator('a', { hasText: 'Devices' }).locator('visible=true').first();
-	await devicesNav.click();
+			const confirmButton = page.locator('button').filter({ hasText: 'Delete' }).nth(0);
+			await confirmButton.click();
 
-	await expect(page).toHaveScreenshot();
-});
+			await expect(page).toHaveScreenshot();
 
-test('explores more pages', async ({ page }) => {
-	// Navigate to the Devices page
-	await page.goto('/devices');
-	await expect(page).toHaveScreenshot();
+			const devicesNav = page.locator('a', { hasText: 'Devices' }).locator('visible=true').first();
+			await devicesNav.click();
 
-	// Navigate to the Tags page
-	const tagsNav = page.locator('a', { hasText: 'Tags' }).locator('visible=true').first();
-	await tagsNav.click();
-	await expect(page).toHaveScreenshot();
+			await expect(page).toHaveScreenshot();
+		});
 
-	// Navigate to the History page
-	const historyNav = page.locator('a', { hasText: 'History' }).locator('visible=true').first();
-	await historyNav.click();
-	await expect(page).toHaveScreenshot();
+		test('explores more pages', async ({ page }) => {
+			// Navigate to the Devices page
+			await page.goto('/devices');
+			await expect(page).toHaveScreenshot();
 
-	// Navigate to the External Repositories page
-	const externalReposNav = page
-		.locator('a', { hasText: 'External Repositories' })
-		.locator('visible=true')
-		.first();
-	await externalReposNav.click();
-	await expect(page).toHaveScreenshot();
+			// Navigate to the Tags page
+			const tagsNav = page.locator('a', { hasText: 'Tags' }).locator('visible=true').first();
+			await tagsNav.click();
+			await expect(page).toHaveScreenshot();
+
+			// Navigate to the History page
+			const historyNav = page.locator('a', { hasText: 'History' }).locator('visible=true').first();
+			await historyNav.click();
+			await expect(page).toHaveScreenshot();
+
+			// Navigate to the External Repositories page
+			const externalReposNav = page
+				.locator('a', { hasText: 'External Repositories' })
+				.locator('visible=true')
+				.first();
+			await externalReposNav.click();
+			await expect(page).toHaveScreenshot();
+		});
+	});
 });
