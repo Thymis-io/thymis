2e6dd090394b9028fa290243a926a7a4fb3b78ec
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-23 13:52:27 +0100

    tests support dark mode

--- frontend/tests/unauthenticated.spec.ts
+++ frontend/tests/unauthenticated.spec.ts
@@ -3,38 +3,45 @@ import { test, expect } from '../playwright/fixtures';
 // Reset storage state for this file to avoid being authenticated
 test.use({ storageState: { cookies: [], origins: [] } });
 
-test('index page redirects to login', async ({ page }) => {
-	const resp = await page.goto('/');
-	expect(resp?.status()).toBe(200);
-	expect(resp?.url()).toBe('http://localhost:8000/login');
-});
+const colorSchemes = ['light', 'dark'] as const;
 
-test('login page shows login form', async ({ page }) => {
-	const resp = await page.goto('/login');
-	expect(resp?.status()).toBe(200);
+colorSchemes.forEach((colorScheme) => {
+	test.describe(`Color scheme: ${colorScheme}`, () => {
+		test.use({ colorScheme: colorScheme });
+		test('index page redirects to login', async ({ page }) => {
+			const resp = await page.goto('/');
+			expect(resp?.status()).toBe(200);
+			expect(resp?.url()).toBe('http://localhost:8000/login');
+		});
 
-	await expect(page.locator('input[name="username"]')).toBeVisible();
-	await expect(page.locator('input[name="password"]')).toBeVisible();
-	await expect(page.locator('button[type="submit"]')).toBeVisible();
+		test('login page shows login form', async ({ page }) => {
+			const resp = await page.goto('/login');
+			expect(resp?.status()).toBe(200);
 
-	await expect(page).toHaveScreenshot();
-});
+			await expect(page.locator('input[name="username"]')).toBeVisible();
+			await expect(page.locator('input[name="password"]')).toBeVisible();
+			await expect(page.locator('button[type="submit"]')).toBeVisible();
 
-test('can login with testadminpassword', async ({ page }) => {
-	await page.goto('/login');
+			await expect(page).toHaveScreenshot();
+		});
 
-	await page.fill('input[name="username"]', 'admin');
-	await page.fill('input[name="password"]', 'testadminpassword');
-	await page.click('button[type="submit"]');
+		test('can login with testadminpassword', async ({ page }) => {
+			await page.goto('/login');
 
-	await page.waitForURL('http://localhost:8000/overview');
+			await page.fill('input[name="username"]', 'admin');
+			await page.fill('input[name="password"]', 'testadminpassword');
+			await page.click('button[type="submit"]');
 
-	expect(page.url()).toBe('http://localhost:8000/overview');
-});
+			await page.waitForURL('http://localhost:8000/overview');
 
-test('visiting overview page without login redirects to login', async ({ page }) => {
-	const resp = await page.goto('/overview');
+			expect(page.url()).toBe('http://localhost:8000/overview');
+		});
 
-	expect(resp?.status()).toBe(200);
-	expect(resp?.url()).toBe('http://localhost:8000/login?redirect=%2Foverview');
+		test('visiting overview page without login redirects to login', async ({ page }) => {
+			const resp = await page.goto('/overview');
+
+			expect(resp?.status()).toBe(200);
+			expect(resp?.url()).toBe('http://localhost:8000/login?redirect=%2Foverview');
+		});
+	});
 });
