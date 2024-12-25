faca4f1daeeae1cea37abcce56ba71e1ed452e51
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-12-25 23:59:00 +0100

    chore: created own pagination component

---
+++ frontend/src/lib/components/Paginator.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+
+	export let totalCount: number;
+	export let pageSize: number;
+	export let limit: number = 2;
+	export let page: number;
+	export let onChange: (page: number) => void;
+
+	$: pageCount = Math.ceil(totalCount / pageSize);
+	$: visiblePagesCount = limit * 2 + 1;
+	$: visiblePagesStart = Math.max(Math.min(page - limit - 1, pageCount - visiblePagesCount), 0);
+	$: visiblePages = Array.from({ length: pageCount }, (_, i) => i).slice(
+		visiblePagesStart,
+		visiblePagesStart + visiblePagesCount
+	);
+
+	const handlePageChange = (page: number) => {
+		if (page < 1 || page > pageCount) return;
+		onChange(page);
+	};
+</script>
+
+<div class="flex items-center paginate">
+	<button on:click={() => handlePageChange(page - 1)} class="page-button">
+		<ChevronLeft></ChevronLeft>
+	</button>
+	{#each visiblePages as i}
+		<button
+			on:click={() => handlePageChange(i + 1)}
+			class={'page-button'}
+			class:active={i + 1 === page}
+		>
+			{i + 1}
+		</button>
+	{/each}
+	<button on:click={() => handlePageChange(page + 1)} class="page-button">
+		<ChevronRight></ChevronRight>
+	</button>
+</div>
+
+<style lang="postcss">
+	.page-button {
+		border: 1px solid rgb(128, 128, 128);
+		padding: 0.1rem;
+		min-width: 1.5rem;
+	}
+	.paginate :first-child {
+		border-radius: 0.3rem 0 0 0.3rem;
+	}
+	.paginate :last-child {
+		border-radius: 0 0.3rem 0.3rem 0;
+	}
+	.page-button.active {
+		@apply bg-gray-300;
+		@apply text-primary-500;
+	}
+	.page-button.active:is(.dark *) {
+		@apply bg-gray-500;
+	}
+	.page-button:hover {
+		@apply bg-gray-300;
+	}
+	.page-button:hover:is(.dark *) {
+		@apply bg-gray-500;
+	}
+</style>
