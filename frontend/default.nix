1bd2675addae4b720f170a9aaae1e48361a15519
Eli Kogan-Wang <elikowa@gmail.com>
2024-12-25 01:47:06 +0100

    remove dep

--- frontend/default.nix
+++ frontend/default.nix
@@ -7,7 +7,7 @@ buildNpmPackage {
   pname = "thymis-frontend";
   version = "0.0.1";
   src = ./.;
-  npmDepsHash = "sha256-jxJudkfW6yadw5Z3MKrVVGqpSQ5hA7WsdY1rIG0fiMY=";
+  npmDepsHash = "sha256-Wm9vl/fV1AVE0TthekYxoBkwCDj5R4WrsMHbbJIBy/w=";
   postInstall = ''
     mkdir -p $packageOut/build
     cp -r ./build/* $packageOut/build
