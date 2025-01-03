8a852409ef838202b38719809f493d7743d2190e
Eli Kogan-Wang <elikowa@gmail.com>
2025-01-03 18:19:36 +0100

    fix: compare available settings using JSON.stringify to ensure accurate change detection

--- frontend/src/lib/config/ConfigSelectOne.svelte
+++ frontend/src/lib/config/ConfigSelectOne.svelte
@@ -21,7 +21,7 @@
 	$: extraData = setting.type.extra_data;
 
 	let available_settings = setting.type['select-one'];
-	let last_available_settings = available_settings;
+	let last_available_settings = JSON.stringify(available_settings);
 
 	$: {
 		if (extraData && 'restrict_values_on_other_key' in extraData) {
@@ -48,12 +48,12 @@
 			// if current value is not in available settings, set it to the first available, only if available_settings just changed
 			if (
 				!available_settings.map((option) => option[1]).includes(value) &&
-				available_settings !== last_available_settings
+				JSON.stringify(available_settings) !== last_available_settings
 			) {
 				value = available_settings?.[0]?.[1];
 				if (browser) onChange(value);
 			}
-			last_available_settings = available_settings;
+			last_available_settings = JSON.stringify(available_settings);
 		} else {
 			available_settings = setting.type['select-one'];
 		}
