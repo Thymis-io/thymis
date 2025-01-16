b19f1561ec26c31582c8d4301d0a399441c46eca
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-01-16 14:56:40 +0100

    fix: delete device redirect

--- frontend/src/routes/(authenticated)/configuration/+layout.svelte
+++ frontend/src/routes/(authenticated)/configuration/+layout.svelte
@@ -34,7 +34,7 @@
 </script>
 
 <div class="flex justify-between mb-4">
-	<h1 class="text-3xl font-bold dark:text-white">{$globalNavSelectedTarget?.displayName}</h1>
+	<h1 class="text-3xl font-bold dark:text-white">{$globalNavSelectedTarget?.displayName ?? ''}</h1>
 	<DeployActions />
 </div>
 <Tabbar />
