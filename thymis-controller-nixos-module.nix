2c9e49f2e6be35a29c99ad114d5e746b14783a61
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-01-21 16:06:50 +0100

    fix: increase nginx buffer size

--- thymis-controller-nixos-module.nix
+++ thymis-controller-nixos-module.nix
@@ -98,6 +98,9 @@ in
             proxy_http_version 1.1;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection $connection_upgrade;
+            proxy_buffer_size          128k;
+            proxy_buffers            4 256k;
+            proxy_busy_buffers_size    256k;
           '';
         };
       };
