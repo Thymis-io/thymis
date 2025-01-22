61ad87f546ada4aed271695c1cd81f2803be89ea
Eli Kogan-Wang <elikowa@gmail.com>
2025-01-22 01:46:52 +0100

    fix: redirect to devices page if no deployment infos are found

--- frontend/src/routes/(authenticated)/configuration/configuration-details/+page.ts
+++ frontend/src/routes/(authenticated)/configuration/configuration-details/+page.ts
@@ -1,11 +1,15 @@
 import type { PageLoad } from './$types';
 import { getDeploymentInfosByConfigId, type DeploymentInfo } from '$lib/deploymentInfo';
+import { redirect } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ fetch, url }) => {
 	let deploymentInfos: DeploymentInfo[];
 	const identifier = url.searchParams.get('global-nav-target');
 	if (identifier) {
 		deploymentInfos = await getDeploymentInfosByConfigId(fetch, identifier);
+		if (deploymentInfos.length === 0) {
+			redirect(302, '/devices');
+		}
 		return { deploymentInfos: deploymentInfos };
 	} else {
 		throw new Error('No identifier provided');
