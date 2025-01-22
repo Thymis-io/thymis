b915e70f2865d7a8c7f2669ba2e8e9be4200da00
Eli Kogan-Wang <elikowa@gmail.com>
2025-01-22 03:11:33 +0100

    fix: enhance configuration details loading logic to redirect if device not found in parent state

--- frontend/src/routes/(authenticated)/configuration/+layout.svelte
+++ frontend/src/routes/(authenticated)/configuration/+layout.svelte
@@ -1,36 +1,7 @@
 <script lang="ts">
-	import { t } from 'svelte-i18n';
-	import { Card } from 'flowbite-svelte';
-	import ModuleList from '$lib/config/ModuleList.svelte';
-	import {
-		getDeviceByIdentifier,
-		getTagByIdentifier,
-		globalNavSelectedTargetType,
-		saveState
-	} from '$lib/state';
-	import {
-		type ModuleSettings,
-		type ModuleSettingsWithOrigin,
-		type Tag,
-		type Device,
-		type Module,
-		type Origin,
-		globalNavSelectedTarget,
-		state,
-		globalNavSelectedTag,
-		globalNavSelectedConfig
-	} from '$lib/state';
+	import { globalNavSelectedTarget } from '$lib/state';
 	import DeployActions from '$lib/components/DeployActions.svelte';
-	import type { PageData } from './$types';
-	import ConfigModuleCard from '$lib/config/ModuleCard.svelte';
-	import HardDrive from 'lucide-svelte/icons/hard-drive';
-	import TagIcon from 'lucide-svelte/icons/tag';
-	import { queryParam } from 'sveltekit-search-params';
-	import { derived } from 'svelte/store';
-	import { page } from '$app/stores';
 	import Tabbar from '$lib/components/Tabbar.svelte';
-
-	export let data: PageData;
 </script>
 
 <div class="flex justify-between mb-4">
--- frontend/src/routes/(authenticated)/configuration/configuration-details/+page.ts
+++ frontend/src/routes/(authenticated)/configuration/configuration-details/+page.ts
@@ -1,14 +1,16 @@
-import type { PageLoad } from './$types';
+import type { PageLoad, PageParentData } from './$types';
 import { getDeploymentInfosByConfigId, type DeploymentInfo } from '$lib/deploymentInfo';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ fetch, url }) => {
+export const load: PageLoad = async ({ fetch, url, parent }) => {
 	let deploymentInfos: DeploymentInfo[];
 	const identifier = url.searchParams.get('global-nav-target');
 	if (identifier) {
 		deploymentInfos = await getDeploymentInfosByConfigId(fetch, identifier);
-		if (deploymentInfos.length === 0) {
-			redirect(302, '/devices');
+		const parentData: PageParentData = await parent();
+		// if config is not in parentData state, redirect to '/devices'
+		if (!parentData.state.devices.find((device) => device.identifier === identifier)) {
+			redirect(303, '/devices');
 		}
 		return { deploymentInfos: deploymentInfos };
 	} else {
