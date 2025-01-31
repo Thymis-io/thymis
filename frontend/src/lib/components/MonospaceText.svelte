8f5f463e08076598867aa7e3f5cc1e1d964b6e38
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-01-31 22:22:07 +0100

    feat: copy code block

--- frontend/src/lib/components/MonospaceText.svelte
+++ frontend/src/lib/components/MonospaceText.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
+	import { t } from 'svelte-i18n';
+	import Copy from 'lucide-svelte/icons/copy';
 	import { HighlightAuto, LineNumbers } from 'svelte-highlight';
 	import ashes from 'svelte-highlight/styles/ashes';
 
 	export let code;
+
+	let copied = false;
+
+	const copy = () => {
+		navigator.clipboard.writeText(code);
+		copied = true;
+		setTimeout(() => {
+			copied = false;
+		}, 3000);
+	};
 </script>
 
 <svelte:head>
 	{@html ashes}
 </svelte:head>
 
-<HighlightAuto {code} let:highlighted>
-	<LineNumbers {highlighted} />
-</HighlightAuto>
+<div class="relative">
+	<HighlightAuto {code} let:highlighted>
+		<LineNumbers {highlighted} />
+	</HighlightAuto>
+	<div class="absolute top-2 right-2">
+		<button
+			class="absolute right-0 flex items-center gap-2 z-20 text-white hover:bg-gray-700 rounded-md p-1 w-max h-max opacity-30 hover:opacity-100"
+			on:click={copy}
+		>
+			<Copy class="w-5 h-5" />
+			{#if copied}
+				{$t('codeblock.copied')}
+			{:else}
+				{$t('codeblock.copy')}
+			{/if}
+		</button>
+	</div>
+</div>
