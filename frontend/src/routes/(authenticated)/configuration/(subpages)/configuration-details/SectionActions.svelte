594f701a8d5554ba78f3649bc7024e7e445c3cc9
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-02-12 19:08:16 +0100

    feat: move download image to config subpage

--- frontend/src/routes/(authenticated)/configuration/(subpages)/configuration-details/SectionActions.svelte
+++ frontend/src/routes/(authenticated)/configuration/(subpages)/configuration-details/SectionActions.svelte
@@ -17,32 +17,11 @@
 		});
 	};
 
-	const buildAndDownloadImage = async (config: Config) => {
-		await fetchWithNotify(`/api/action/build-download-image?identifier=${config.identifier}`, {
-			method: 'POST'
-		});
-	};
-
 	let className = '';
 	export { className as class };
-
-	$: image_format = getConfigImageFormat(config);
 </script>
 
 <Section class={className} title={$t('configuration-details.actions')}>
-	<Button
-		class="px-2 py-1.5 gap-2 flex justify-start"
-		color="alternative"
-		on:click={() => buildAndDownloadImage(config)}
-	>
-		{#if image_format == 'nixos-vm'}
-			<Play size={'1rem'} class="min-w-4" />
-			{$t('configurations.actions.build_vm_and_start')}
-		{:else}
-			<Download size={'1rem'} class="min-w-4" />
-			{$t('configurations.actions.download')}
-		{/if}
-	</Button>
 	<Button
 		class="px-2 py-1.5 gap-2 justify-start"
 		color="alternative"
