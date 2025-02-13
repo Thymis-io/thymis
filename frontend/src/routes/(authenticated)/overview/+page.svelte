33759b743e5de286357176b5fab5f1ed8180fa3f
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-02-13 12:59:02 +0100

    fix: mobile login screen

--- frontend/src/routes/(authenticated)/overview/+page.svelte
+++ frontend/src/routes/(authenticated)/overview/+page.svelte
@@ -10,7 +10,7 @@
 
 <PageHead title={$t('nav.overview')} />
 <!-- container for a bunch of cards -->
-<div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+<div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
 	<Card class="card p-4 bg-white rounded-lg shadow-md w-full">
 		<div class="flex items-center justify-around">
 			<div class="flex items-center justify-center bg-primary-500 rounded-full p-5 w-full">
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -23,7 +23,7 @@
 			authenticated={false}
 		/>
 	</header>
-	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+	<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
 		<div
 			class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
 		>
