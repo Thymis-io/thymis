1df04d27015296080005ff6ebcede47c24787eb6
Eli Kogan-Wang <elikowa@gmail.com>
2025-02-15 23:31:25 +0100

    add wpa enterprise and ca root support

--- controller/thymis_controller/modules/builtin_modules.py
+++ controller/thymis_controller/modules/builtin_modules.py
@@ -1,12 +1,11 @@
 from .kiosk import Kiosk
 from .modules import Module
 from .oci_container import OCIContainers
-from .thymis import ThymisController, ThymisDevice
+from .thymis import ThymisDevice
 from .whatever import WhateverModule
 
 ALL_MODULES: list[Module] = [
     ThymisDevice(),
-    ThymisController(),
     WhateverModule(),
     Kiosk(),
     OCIContainers(),
