9fc74226567e7776039b74cd643b74d38d96fbb8
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-02-17 10:20:11 +0100

    chore: simplify task output

--- controller/thymis_controller/task/subscribe_ui.py
+++ controller/thymis_controller/task/subscribe_ui.py
@@ -37,13 +37,7 @@ class SubscribedTask(TaskMessage):
 
 class SubscribedTaskOutput(TaskMessage):
     type: str = "subscribed_task_output"
-    stdout: str | None
-    stderr: str | None
-    nix_errors: list[ErrorInfoNixLine] | None
-    nix_error_logs: list[str] | None
-    nix_warning_logs: list[str] | None
-    nix_notice_logs: list[str] | None
-    nix_info_logs: list[str] | None
+    task: models.Task
 
 
 class TaskWebsocketSubscriber:
@@ -58,6 +52,8 @@ class TaskWebsocketSubscriber:
         self.db_engine = db_engine
         self.controller = controller
         self.websocket = websocket
+        self.stdout_count = 0
+        self.stderr_count = 0
         self.nix_errors_count = 0
         self.nix_error_logs_count = 0
         self.nix_warning_logs_count = 0
@@ -73,7 +69,6 @@ class TaskWebsocketSubscriber:
         self.controller.executor.on_new_task.unsubscribe(self.notify_new_task)
         self.controller.executor.on_task_update.unsubscribe(self.notify_task_update)
         self.controller.executor.on_task_output.unsubscribe(self.notify_task_output)
-        self.task_queue.shutdown()
 
     async def run(self):
         send_task_coroutine = self.loop.create_task(self.websocket_loop(self.send_task))
@@ -88,8 +83,7 @@ class TaskWebsocketSubscriber:
                 await inner()
             except (WebSocketDisconnect, asyncio.QueueShutDown):
                 break
-            except Exception as e:
-                logger.warning(f"Unexpected Exception in websocket_loop: {type(e)}")
+            except Exception:
                 break
 
     async def send_task(self):
@@ -106,26 +100,46 @@ class TaskWebsocketSubscriber:
             async with self.send_lock:
                 with self.process_subscribed_task_lock:
                     self.subscribed_task = task_id
-                    subscribed_task = self.create_initial_subscribed_task_message(
-                        task_id
+                    self.stdout_count = 0
+                    self.stderr_count = 0
+                    self.nix_errors_count = 0
+                    self.nix_error_logs_count = 0
+                    self.nix_warning_logs_count = 0
+                    self.nix_notice_logs_count = 0
+                    self.nix_info_logs_count = 0
+                    with Session(self.db_engine) as db_session:
+                        task = self.controller.get_task(task_id, db_session)
+                    subscribed_task = self.create_subscribed_task_message(task)
+                await self.websocket.send_json(
+                    SubscribedTask(task_id=task.id, task=subscribed_task).model_dump(
+                        mode="json"
                     )
-                await self.websocket.send_json(subscribed_task.model_dump(mode="json"))
+                )
 
-    def create_initial_subscribed_task_message(self, task_id: uuid.UUID):
-        with Session(self.db_engine) as db_session:
-            task = self.controller.get_task(task_id, db_session)
-        self.nix_errors_count = len(task.nix_errors) if task.nix_errors else 0
-        self.nix_error_logs_count = (
-            len(task.nix_error_logs) if task.nix_error_logs else 0
-        )
-        self.nix_warning_logs_count = (
-            len(task.nix_warning_logs) if task.nix_warning_logs else 0
-        )
-        self.nix_notice_logs_count = (
-            len(task.nix_notice_logs) if task.nix_notice_logs else 0
-        )
-        self.nix_info_logs_count = len(task.nix_info_logs) if task.nix_info_logs else 0
-        return SubscribedTask(task_id=task_id, task=task)
+    def create_subscribed_task_message(self, db_task: db_models.Task):
+        task = models.Task.from_orm_task(db_task)
+        if task.process_stdout:
+            task.process_stdout = task.process_stdout[self.stdout_count :]
+            self.stdout_count = len(task.process_stdout)
+        if task.process_stderr:
+            task.process_stderr = task.process_stderr[self.stderr_count :]
+            self.stderr_count = len(task.process_stderr)
+        if task.nix_errors:
+            task.nix_errors = task.nix_errors[self.nix_errors_count :]
+            self.nix_errors_count = len(task.nix_errors)
+        if task.nix_error_logs:
+            task.nix_error_logs = task.nix_error_logs[self.nix_error_logs_count :]
+            self.nix_error_logs_count = len(task.nix_error_logs)
+        if task.nix_warning_logs:
+            task.nix_warning_logs = task.nix_warning_logs[self.nix_warning_logs_count :]
+            self.nix_warning_logs_count = len(task.nix_warning_logs)
+        if task.nix_notice_logs:
+            task.nix_notice_logs = task.nix_notice_logs[self.nix_notice_logs_count :]
+            self.nix_notice_logs_count = len(task.nix_notice_logs)
+        if task.nix_info_logs:
+            task.nix_info_logs = task.nix_info_logs[self.nix_info_logs_count :]
+            self.nix_info_logs_count = len(task.nix_info_logs)
+        return task
 
     def enqueue_task(self, task_message: TaskMessage):
         self.loop.call_soon_threadsafe(self.task_queue.put_nowait, task_message)
@@ -138,43 +152,10 @@ class TaskWebsocketSubscriber:
         short_task = crud.task.TaskShort.from_orm_task(task)
         self.enqueue_task(ShortTaskUpdate(task_id=task.id, task=short_task))
 
-    def notify_task_output(
-        self,
-        task: db_models.Task,
-        stdout: str | None,
-        stderr: str | None,
-        nix_status: ParsedNixProcess | None,
-    ):
+    def notify_task_output(self, task: db_models.Task):
         with self.process_subscribed_task_lock:
             if self.subscribed_task == task.id:
-                if nix_status:
-                    logs_by_level = nix_status.logs_by_level
-                    nix_errors = nix_status.errors[self.nix_errors_count :]
-                    nix_error_logs = logs_by_level[0][self.nix_error_logs_count :]
-                    nix_warning_logs = logs_by_level[1][self.nix_warning_logs_count :]
-                    nix_notice_logs = logs_by_level[2][self.nix_notice_logs_count :]
-                    nix_info_logs = logs_by_level[3][self.nix_info_logs_count :]
-                    self.nix_errors_count = len(nix_status.errors)
-                    self.nix_error_logs_count = len(logs_by_level[0])
-                    self.nix_warning_logs_count = len(logs_by_level[1])
-                    self.nix_notice_logs_count = len(logs_by_level[2])
-                    self.nix_info_logs_count = len(logs_by_level[3])
-                else:
-                    nix_errors = None
-                    nix_error_logs = None
-                    nix_warning_logs = None
-                    nix_notice_logs = None
-                    nix_info_logs = None
-
+                subscribed_task = self.create_subscribed_task_message(task)
                 self.enqueue_task(
-                    SubscribedTaskOutput(
-                        task_id=task.id,
-                        stdout=stdout,
-                        stderr=stderr,
-                        nix_errors=nix_errors,
-                        nix_error_logs=nix_error_logs,
-                        nix_warning_logs=nix_warning_logs,
-                        nix_notice_logs=nix_notice_logs,
-                        nix_info_logs=nix_info_logs,
-                    )
+                    SubscribedTaskOutput(task_id=task.id, task=subscribed_task)
                 )
