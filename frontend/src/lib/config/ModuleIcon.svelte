5b2009b2f373d439b7da53c0fd5267d367289ea7
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-02-24 13:05:36 +0100

    fix: use default ModuleIcon as last fallback

--- frontend/src/lib/config/ModuleIcon.svelte
+++ frontend/src/lib/config/ModuleIcon.svelte
@@ -5,12 +5,12 @@
 </script>
 
 <img
-	src={module.icon ?? '/ModuleIcon.svg'}
+	src={module.icon ?? module.iconDark ?? '/ModuleIcon.svg'}
 	alt={module.displayName}
 	class="w-5 block dark:hidden"
 />
 <img
-	src={module.iconDark ?? '/ModuleIcon_dark.svg'}
+	src={module.iconDark ?? module.icon ?? '/ModuleIcon_dark.svg'}
 	alt={module.displayName}
 	class="w-5 hidden dark:block"
 />
