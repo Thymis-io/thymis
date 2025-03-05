c7cb09845d01cd2b687e3e47ca77ea3c99ecef1b
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-03-05 16:00:20 +0100

    fix: use sidebar instead of goto in screencap tests

--- frontend/tests/utils.ts
+++ frontend/tests/utils.ts
@@ -160,7 +160,7 @@ export const createConfiguration = async (
 	deviceType: string,
 	tags: string[]
 ) => {
-	await page.goto('/configuration/list');
+	await page.locator('nav:visible').locator('a', { hasText: 'Configs' }).click();
 
 	const addConfigurationButton = page
 		.locator('button')
@@ -190,7 +190,7 @@ export const createConfiguration = async (
 };
 
 export const createTag = async (page: Page, name: string) => {
-	await page.goto('/tags');
+	await page.locator('nav:visible').locator('a', { hasText: 'Config-Tags' }).click();
 
 	const addTagButton = page.locator('button').filter({ hasText: 'Create Tag' });
 	await addTagButton.click();
