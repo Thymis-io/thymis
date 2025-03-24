654600e9e8829e49cf4910e32c83c1d8b1abc1e1
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-03-24 16:09:35 +0100

    feat: delete artifacs

--- frontend/src/routes/(authenticated)/artifacts/[...rest]/Tree.svelte
+++ frontend/src/routes/(authenticated)/artifacts/[...rest]/Tree.svelte
@@ -6,7 +6,9 @@
 	import ChevronRight from 'lucide-svelte/icons/chevron-right';
 	import FileIcon from 'lucide-svelte/icons/file';
 	import FolderIcon from 'lucide-svelte/icons/folder';
+	import Trash from 'lucide-svelte/icons/trash';
 	import { goto, invalidate } from '$app/navigation';
+	import DeleteConfirm from '$lib/components/DeleteConfirm.svelte';
 
 	interface Props {
 		artifacts: (Folder | File)[];
@@ -18,10 +20,31 @@
 	let hidden = $state<string[]>([]);
 	let drag = $state<Artifact>();
 
+	let pressedKeys = $state<{ [key: string]: boolean }>({});
+	let deleteConfirmTarget = $state<Artifact | undefined>();
+
 	let elementClass =
 		'flex items-center gap-1 w-full text-base ' +
 		'hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer rounded p-1 text-start whitespace-nowrap ';
 	let overflowClass = 'overflow-hidden text-ellipsis ';
+
+	$effect(() => {
+		const keydownHandler = (event: KeyboardEvent) => {
+			pressedKeys[event.key] = true;
+		};
+
+		const keyupHandler = (event: KeyboardEvent) => {
+			delete pressedKeys[event.key];
+		};
+
+		window.addEventListener('keydown', keydownHandler);
+		window.addEventListener('keyup', keyupHandler);
+
+		return () => {
+			window.removeEventListener('keydown', keydownHandler);
+			window.removeEventListener('keyup', keyupHandler);
+		};
+	});
 
 	const dropHandler = async (event: DragEvent) => {
 		event.preventDefault();
@@ -70,15 +93,39 @@
 			drag = undefined;
 		}
 	};
+
+	const clickDelete = (e: MouseEvent, artifact: Artifact) => {
+		e.stopPropagation();
+		e.preventDefault();
+		if (pressedKeys['Shift']) {
+			deleteArtifact(artifact);
+		} else {
+			deleteConfirmTarget = artifact;
+		}
+	};
+
+	const deleteArtifact = async (artifact: Artifact) => {
+		await fetch(`/api/artifacts/${artifact.path}`, {
+			method: 'DELETE'
+		});
+
+		await invalidate((url) => url.pathname.startsWith('/api/artifacts'));
+	};
 </script>
 
+<DeleteConfirm
+	target={deleteConfirmTarget?.name}
+	description={$t('deleteConfirm.text', { values: { target: deleteConfirmTarget?.path } })}
+	on:cancel={() => (deleteConfirmTarget = undefined)}
+	on:confirm={() => deleteConfirmTarget && deleteArtifact(deleteConfirmTarget)}
+/>
 {#each artifacts as artifact}
 	{#if artifact.type === 'folder'}
 		<div
 			role="button"
 			tabindex="0"
 			class={elementClass +
-				'drop-zone ' +
+				'drop-zone hover-visible-parent ' +
 				(drag?.name === artifact?.name ? 'bg-gray-100 dark:bg-gray-700 ' : ' ')}
 			onclick={() => {
 				hidden = hidden.includes(artifact.name)
@@ -105,6 +152,13 @@
 			{/if}
 			<FolderIcon class="w-4 h-4 shrink-0" />
 			<span class={overflowClass}>{artifact.name}</span>
+			<button
+				class={'ml-auto hover-visible-child rounded bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-500 ' +
+					(pressedKeys['Shift'] ? 'text-red-500 ' : '')}
+				onclick={(e) => clickDelete(e, artifact)}
+			>
+				<Trash size="20" />
+			</button>
 		</div>
 		{#if !hidden.includes(artifact.name)}
 			<Tree artifacts={artifact.children} depth={depth + 1} />
@@ -114,7 +168,7 @@
 			role="button"
 			tabindex="0"
 			class={elementClass +
-				'drop-zone ' +
+				'drop-zone hover-visible-parent ' +
 				(drag?.name === artifact?.name ? 'bg-gray-100 dark:bg-gray-700 ' : ' ')}
 			onclick={() => goto(`/artifacts/${artifact.path}`)}
 			onkeydown={(event) => {
@@ -131,6 +185,13 @@
 			<div class="w-4 h-4 shrink-0"></div>
 			<FileIcon class="w-4 h-4 shrink-0" />
 			<span class={overflowClass}>{artifact.name}</span>
+			<button
+				class={'ml-auto hover-visible-child rounded bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-500 ' +
+					(pressedKeys['Shift'] ? 'text-red-500 ' : '')}
+				onclick={(e) => clickDelete(e, artifact)}
+			>
+				<Trash size="18" />
+			</button>
 		</div>
 	{/if}
 {/each}
@@ -138,5 +199,14 @@
 <style>
 	.drop-zone * {
 		pointer-events: none;
+	}
+
+	.hover-visible-parent > .hover-visible-child {
+		display: none;
+	}
+
+	.hover-visible-parent:hover > .hover-visible-child {
+		display: block;
+		pointer-events: auto;
 	}
 </style>
