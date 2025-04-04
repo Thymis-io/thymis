2c8e2bf0baeaccb581a2cb8861ea2c77d7b14e1f
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-04-04 15:49:50 +0200

    fix: use fixed version min width for screenshots

--- frontend/src/lib/taskbar/TaskbarSmall.svelte
+++ frontend/src/lib/taskbar/TaskbarSmall.svelte
@@ -39,45 +39,50 @@
 		headRev: string;
 		dirty: boolean;
 	} = __THYMIS_PACKAGE_VERSION__; // ts-ignore
+
+	const shortRev = inPlaywright ? '00000000' : versionInfo.headRev.slice(0, 8);
 </script>
 
 <div
 	class="border-2 dark:border-0 w-full h-full flex px-2 gap-2 sm:gap-4 xl:gap-10 pr-8 md:pr-16 items-center bg-gray-50 dark:bg-gray-700 overflow-y-auto"
 >
 	<div class="text-xs md:text-sm playwright-snapshot-unstable">
-		<span class="hidden lg:inline">
+		<span class="hidden xl:block min-w-[14rem]">
 			{$t('taskbar.version', { values: { version: versionInfo.version } })}
+			(<span class="font-mono">
+				{shortRev}
+			</span>{versionInfo.dirty ? '-dirty' : ''})
 		</span>
-		<span class="inline lg:hidden">
+		<span class="block xl:hidden">
 			{$t('taskbar.version-short', { values: { version: versionInfo.version } })}
+			(<span class="font-mono">
+				{shortRev}
+			</span>{versionInfo.dirty ? '-dirty' : ''})
 		</span>
-		(<span class="font-mono">
-			{#if inPlaywright}00000000{:else}{versionInfo.headRev.slice(0, 8)}{/if}
-		</span>{versionInfo.dirty ? '-dirty' : ''})
 	</div>
-	<TaskbarIcon class="ml-auto" title={$t('taskbar.pending')} tasks={pendingTasks}>
+	<TaskbarIcon class="hidden md:flex ml-auto" title={$t('taskbar.pending')} tasks={pendingTasks}>
 		{#snippet icon()}
 			<PendingIcon size={20} class="w-full" />
 		{/snippet}
 	</TaskbarIcon>
-	<TaskbarIcon title={$t('taskbar.running')} tasks={runningTasks}>
+	<TaskbarIcon class="hidden md:flex" title={$t('taskbar.running')} tasks={runningTasks}>
 		{#snippet icon()}
 			<RunningIcon size={20} class="w-full" />
 		{/snippet}
 	</TaskbarIcon>
-	<TaskbarIcon title={$t('taskbar.completed')} tasks={completedTasks}>
+	<TaskbarIcon class="hidden md:flex" title={$t('taskbar.completed')} tasks={completedTasks}>
 		{#snippet icon()}
 			<CompletedIcon size={20} class="w-full" />
 		{/snippet}
 	</TaskbarIcon>
-	<TaskbarIcon title={$t('taskbar.failed')} tasks={failedTasks}>
+	<TaskbarIcon class="hidden md:flex" title={$t('taskbar.failed')} tasks={failedTasks}>
 		{#snippet icon()}
 			<FailedIcon size={20} class="w-full" />
 		{/snippet}
 	</TaskbarIcon>
 	<div class="flex items-center gap-1 lg:gap-2 lg:ml-2">
 		{#if latestTask}
-			<span class="text-xs md:text-sm mr-1">
+			<span class="text-xs md:text-sm">
 				{$t('taskbar.latest-task')}:
 			</span>
 			<span class="text-xs md:text-sm truncate max-w-64">
