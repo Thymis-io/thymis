3522c8f5117a6e70f852967b3591ba5acc31f18c
Eli Kogan-Wang <elikowa@gmail.com>
2025-04-30 10:28:10 +0200

    refactor: remove commented-out LogEntry model and unused log data

--- controller/thymis_controller/models/logs.py
+++ controller/thymis_controller/models/logs.py
@@ -1,5 +1,3 @@
-# Got logs data: b'{"message":" Connection closed with error: received 1012 (service restart); then sent 1012 (service restart)","facility":3,"severity":6,"syslogtag":"thymis-agent-start[3149]:","programname":"thymis-agent-start","host":"asdasd","timestamp":"2025-04-29T15:47:30.895708+02:00","uuid":"7910600A94F24FE4B7A1D05AABDFFFBF"}'
-
 import uuid
 from datetime import datetime
 
