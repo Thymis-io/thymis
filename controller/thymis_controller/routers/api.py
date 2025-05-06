9b69bb256bf0396cd44c8a218d008206c34934e9
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-05-06 14:19:09 +0200

    feat: renamed metrics to statistics and add more data points

--- controller/thymis_controller/routers/api.py
+++ controller/thymis_controller/routers/api.py
@@ -5,9 +5,9 @@ from thymis_controller.dependencies import require_valid_user_session
 from thymis_controller.routers import (
     api_action,
     api_deployment_info,
-    api_metrics,
     api_secrets,
     api_state,
+    api_statistics,
     api_task,
     api_ui_sockets,
 )
@@ -24,4 +24,4 @@ router.include_router(api_state.router)
 router.include_router(api_action.router)
 router.include_router(api_deployment_info.router)
 router.include_router(api_ui_sockets.router)
-router.include_router(api_metrics.router)
+router.include_router(api_statistics.router)
---
+++ controller/thymis_controller/routers/api_statistics.py
@@ -0,0 +1,111 @@
+import datetime
+from typing import Annotated, Optional
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from thymis_controller import crud
+from thymis_controller.dependencies import DBSessionAD, NetworkRelayAD, ProjectAD
+
+router = APIRouter()
+
+
+class StatisticParameters(BaseModel):
+    date_from: Optional[datetime.datetime] = None
+    date_to: Optional[datetime.datetime] = None
+
+    def get_date_from(self) -> datetime.datetime:
+        if self.date_from:
+            return self.date_from
+        now = datetime.datetime.now()
+        return now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+    def get_date_to(self) -> datetime.datetime:
+        if self.date_to:
+            return self.date_to
+        now = datetime.datetime.now()
+        if now.month < 12:
+            next_month = now.month + 1
+            next_year = now.year
+        else:
+            next_month = 1
+            next_year = now.year + 1
+        return datetime.datetime(
+            year=next_year,
+            month=next_month,
+            day=1,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0,
+        )
+
+
+@router.get("/statistics", tags=["metrics"])
+def thymis_statistics(
+    params: Annotated[StatisticParameters, Depends()],
+    db_session: DBSessionAD,
+    network_relay: NetworkRelayAD,
+    project: ProjectAD,
+):
+    date_from = params.get_date_from()
+    date_to = params.get_date_to()
+    currently_connected = crud.deployment_info.get_connected_deployment_infos(
+        db_session, network_relay
+    )
+    max_concurrent_connected = crud.agent_connection.get_max_concurrent_connections(
+        db_session, date_from, date_to
+    )
+    tasks_completed = crud.task.get_tasks_with_state(
+        db_session, "completed", date_from, date_to
+    )
+    tasks_failed = crud.task.get_tasks_with_state(
+        db_session, "failed", date_from, date_to
+    )
+    state = project.read_state()
+
+    return {
+        "date_from": date_from,
+        "date_to": date_to,
+        "metrics": [
+            {
+                "name": "currently_connected_devices_count",
+                "value": len(currently_connected),
+                "respect_time_frame": False,
+            },
+            {
+                "name": "max_concurrent_connected_devices_count",
+                "value": len(max_concurrent_connected),
+                "respect_time_frame": True,
+            },
+            {
+                "name": "currently_connected_devices",
+                "value": currently_connected,
+                "respect_time_frame": False,
+            },
+            {
+                "name": "max_concurrent_connected_devices",
+                "value": max_concurrent_connected,
+                "respect_time_frame": True,
+            },
+            {
+                "name": "tasks_completed_count",
+                "value": len(tasks_completed),
+                "respect_time_frame": True,
+            },
+            {
+                "name": "tasks_failed_count",
+                "value": len(tasks_failed),
+                "respect_time_frame": True,
+            },
+            {
+                "name": "project_tags_count",
+                "value": len(state.tags),
+                "respect_time_frame": False,
+            },
+            {
+                "name": "project_configs_count",
+                "value": len(state.configs),
+                "respect_time_frame": False,
+            },
+        ],
+    }
