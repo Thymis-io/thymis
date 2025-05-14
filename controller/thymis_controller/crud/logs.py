a8d4894228516f2275561248842ba3a484a82835
Eli Kogan-Wang <elikowa@gmail.com>
2025-05-14 16:08:01 +0200

    add logging in ui WIP

--- controller/thymis_controller/crud/logs.py
+++ controller/thymis_controller/crud/logs.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import nullslast
+from sqlalchemy import nullslast, or_
 from sqlalchemy.dialects.sqlite import insert as sqlite_insert
 from sqlalchemy.orm import Session
 from thymis_controller import db_models
@@ -62,3 +62,29 @@ def create(
     )
     session.execute(stmt)
     session.commit()
+
+
+def get_logs(
+    session: Session,
+    deployment_info: db_models.DeploymentInfo,
+    from_datetime: datetime = None,
+    to_datetime: datetime = None,
+    limit: int = 100,
+    offset: int = 0,
+):
+    # where ID equals or ssh public key equals
+
+    stmt = session.query(db_models.LogEntry)
+    stmt = stmt.filter(
+        or_(
+            db_models.LogEntry.deployment_info_id == deployment_info.id,
+            db_models.LogEntry.ssh_public_key == deployment_info.ssh_public_key,
+        )
+    )
+    if from_datetime is not None:
+        stmt = stmt.filter(db_models.LogEntry.timestamp >= from_datetime)
+    if to_datetime is not None:
+        stmt = stmt.filter(db_models.LogEntry.timestamp <= to_datetime)
+    stmt = stmt.order_by(nullslast(db_models.LogEntry.timestamp.desc()))
+    stmt = stmt.limit(limit).offset(offset)
+    return stmt.all()
