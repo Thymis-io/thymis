81f9593e6368b02dea9c36ddea67a1047c99e90f
Eli Kogan-Wang <elikowa@gmail.com>
2025-07-30 16:20:21 +0200

    chore(terminal): clean up whitespace and formatting in Terminal.svelte

---
+++ docs/svelte.config.js
@@ -0,0 +1,14 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
+	kit: { adapter: adapter() },
+	extensions: ['.svelte', '.svx', '.md'],
+};
+
+export default config;
