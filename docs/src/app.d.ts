57965c63da5fc4e5914f41c28ba185975c9e224b
Eli Kogan-Wang <elikowa@gmail.com>
2025-08-05 16:09:50 +0200

    refactor: add module declaration for Markdown files to enhance type support

--- docs/src/app.d.ts
+++ docs/src/app.d.ts
@@ -10,4 +10,11 @@ declare global {
 	}
 }
 
+declare module '*.md' {
+	import type { SvelteComponent } from 'svelte'
+	const component: typeof SvelteComponent
+	export default component
+	export const metadata: Record<string, unknown>
+}
+
 export {};
