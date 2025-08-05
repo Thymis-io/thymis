c42507175c1c2aed34b10539406e5ac4635e2a51
Eli Kogan-Wang <elikowa@gmail.com>
2025-08-05 16:23:53 +0200

    refactor: enhance documentation layout with summary navigation and styles

--- docs/src/app.d.ts
+++ docs/src/app.d.ts
@@ -8,13 +8,14 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	declare module '*.md' {
+		import type { SvelteComponent } from 'svelte'
+		const component: typeof SvelteComponent
+		export default component
+		export const metadata: Record<string, unknown>
+	}
 }
 
-declare module '*.md' {
-	import type { SvelteComponent } from 'svelte'
-	const component: typeof SvelteComponent
-	export default component
-	export const metadata: Record<string, unknown>
-}
+
 
 export {};
--- docs/src/lib/components/SummaryLayout.svelte
+++ docs/src/lib/components/SummaryLayout.svelte
@@ -2,6 +2,14 @@
 
 </script>
 
+<script context="module">
+  import a from './summary/a.svelte';
+  import li from './summary/li.svelte';
+  import ul from './summary/ul.svelte';
+
+  export { a, li, ul };
+</script>
+
 <slot>
   <!-- the mdsvex content will be slotted in here -->
 </slot>
