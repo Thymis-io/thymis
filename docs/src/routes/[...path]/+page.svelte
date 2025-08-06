056e2220718d1d4c1cf394cd4db0d2c8bb1b2c7f
Eli Kogan-Wang <elikowa@gmail.com>
2025-08-06 14:39:09 +0200

    feat: enhance documentation components with currentPath prop for improved navigation

--- docs/src/routes/[...path]/+page.svelte
+++ docs/src/routes/[...path]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import DocumentationLayout from '../../lib/components/DocumentationLayout.svelte';
 
 	let { data } = $props();
 </script>
 
-<DocumentationLayout path={data.path} />
+<DocumentationLayout path={data.path} currentPath={$page.url.pathname} />
