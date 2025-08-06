f2fdb47d7726f756f3177093dbc0114df5db04ec
Eli Kogan-Wang <elikowa@gmail.com>
2025-08-06 17:02:15 +0200

    fix: update href handling in a.svelte and Footer component for prefix context

--- docs/src/lib/components/defaultMarkdown/a.svelte
+++ docs/src/lib/components/defaultMarkdown/a.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
     import { getContext } from 'svelte';
+    import type { ClassValue } from 'svelte/elements';
+
 
     // get prefix from context
     const prefix = getContext<string>('prefix') || '';
 
-    let { href, children } = $props();
+    // let { href, children,  } = $props();
+    let props =  $props();
 
-    let shouldGetPrefixed = $derived(href.startsWith('/') || href.startsWith('./'));
+    let shouldGetPrefixed = $derived(props.href.startsWith('/') || props.href.startsWith('./'));
 
     let finalHref = $derived(
-        shouldGetPrefixed ? `${prefix}${href}` : href
+        shouldGetPrefixed ? `${prefix}${props.href}` : props.href
     );
 
 </script>
 <a
     href={finalHref}
+    class={props.class}
 >
-    {@render children()}
+    {@render props.children()}
 </a>
