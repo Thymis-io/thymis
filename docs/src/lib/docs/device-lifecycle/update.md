1b3fa985373231a8b91601d28947bb37c4099c88
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-08-11 20:22:52 +0200

    docs: add basic deploy tutorial

--- docs/src/lib/docs/device-lifecycle/update.md
+++ docs/src/lib/docs/device-lifecycle/update.md
@@ -1,1 +1,25 @@
 # Deploy an Update
+
+Updates and config changes can be [deployed](../reference/concepts/deployment.md) to already connected devices.
+
+If an update fails or the device cannot reconnect to the Thymis Controller, the previous configuration will be rolled back automatically.
+
+## Update packages
+
+The **Update** button updates all available packages to their latest versions.
+
+When clicked, this creates an Update Nix Flake task.
+Wait until this task completes before proceeding.
+
+![Update](./Color-scheme-light-deploy-update-1-linux.png)
+
+Once the update task has completed, click the **Deploy** button to push the changes to connected devices.
+
+![Deploy](./Color-scheme-light-deploy-update-2-linux.png)
+
+This opens the deploy modal where the tags/configs to deploy must be selected.
+Next click deploy and wait for the deploy task to complete.
+
+![Deploy Modal](./Color-scheme-light-deploy-update-3-linux.png)
+
+If the deployment fails, see [Troubleshooting](troubleshooting.md).
--- frontend/tests/tutorials.spec.ts
+++ frontend/tests/tutorials.spec.ts
@@ -1,4 +1,4 @@
-import { test, type PageAssertionsToHaveScreenshotOptions } from '../playwright/fixtures';
+import { expect, test, type PageAssertionsToHaveScreenshotOptions } from '../playwright/fixtures';
 import {
 	clearState,
 	deleteAllTasks,
@@ -7,7 +7,8 @@ import {
 } from './utils';
 
 const screenshotOptions: PageAssertionsToHaveScreenshotOptions = {
-	mask: []
+	mask: [],
+	maxDiffPixels: 200
 };
 
 test('initial device provisioning', async ({ page, request }, testInfo) => {
@@ -101,4 +102,119 @@ test('initial device provisioning', async ({ page, request }, testInfo) => {
 		screenshotCounter,
 		screenshotOptions
 	);
+});
+
+test('deploy update', async ({ page, request }, testInfo) => {
+	const screenshotCounter = { count: 0 };
+	const configName = 'Test Configuration';
+
+	page.setViewportSize({ width: 1600, height: 900 });
+
+	await clearState(page, request);
+	await deleteAllTasks(page, request);
+
+	const navConfigLocator = page.locator('nav:visible').locator('a', { hasText: 'Configs' });
+	await navConfigLocator.click();
+
+	const createConfigButton = page.locator('button').filter({ hasText: 'Create New Configuration' });
+	await createConfigButton.click();
+
+	const displayNameInput = page.locator('#display-name').first();
+	await displayNameInput.fill(configName);
+	await displayNameInput.blur();
+
+	const deviceTypeSelect = page.locator('#device-type').first();
+	await deviceTypeSelect.selectOption({ label: 'Generic x86-64' });
+
+	const saveButton = page.locator('button').filter({ hasText: 'create device configuration' });
+	await saveButton.click();
+
+	const configureButton = page.locator('tr').getByRole('button', { name: 'Configure' });
+	await configureButton.click();
+	await page.waitForURL('/configuration/edit*');
+
+	const imageFormatSelect = page
+		.locator('p', { hasText: 'Image Format' })
+		.locator('..')
+		.locator('select')
+		.first();
+	await imageFormatSelect.selectOption({ label: 'NixOS VM' });
+
+	const runVMButton = page.locator('button', { hasText: 'Build and start VM' });
+	await runVMButton.click();
+	const commitButton = page.locator('button').filter({ hasText: 'Commit & Build and start VM' });
+	await commitButton.click();
+
+	// wait until: 1x on screen "completed", 1x on screen "running"
+	test.setTimeout(360000);
+	await page
+		.locator('td', { hasText: 'completed' })
+		.nth(0)
+		.or(page.locator('td', { hasText: 'failed' }).first())
+		.waitFor({ timeout: 360000 });
+	await expect(page.locator('td', { hasText: 'completed' }).nth(0)).toBeVisible();
+
+	await page
+		.locator('td', { hasText: 'running' })
+		.nth(0)
+		.or(page.locator('td', { hasText: 'failed' }).first())
+		.waitFor({ timeout: 360000 });
+	await expect(page.locator('td', { hasText: 'running' }).nth(0)).toBeVisible();
+
+	await navConfigLocator.click();
+
+	const updateButton = page.locator('button').filter({ hasText: 'Update' });
+	await expectScreenshotWithHighlight(
+		page,
+		updateButton,
+		testInfo,
+		screenshotCounter,
+		screenshotOptions
+	);
+
+	await updateButton.click();
+	await page.locator('body').click(); // click to remove focus from the button
+
+	await page
+		.locator('td', { hasText: 'completed' })
+		.nth(1)
+		.or(page.locator('td', { hasText: 'failed' }).first())
+		.waitFor({ timeout: 30000 });
+
+	const deployButton = page.locator('button').filter({ hasText: 'Deploy' });
+	await expectScreenshotWithHighlight(
+		page,
+		deployButton,
+		testInfo,
+		screenshotCounter,
+		screenshotOptions
+	);
+	await deployButton.click();
+	await page.locator('body').click(); // click to remove focus from the button
+
+	const configsToDeploy = page
+		.getByText('Tags/configs to deploy')
+		.locator('..')
+		.locator('input')
+		.first();
+
+	await expectScreenshotWithHighlight(
+		page,
+		configsToDeploy.locator('..'),
+		testInfo,
+		screenshotCounter,
+		screenshotOptions
+	);
+
+	await configsToDeploy.fill(configName);
+	const deployButton2 = page.locator('button').filter({ hasText: 'Deploy' }).nth(1);
+	await deployButton2.click();
+
+	await page
+		.locator('td', { hasText: 'completed' })
+		.nth(3)
+		.or(page.locator('td', { hasText: 'failed' }).first())
+		.waitFor({ timeout: 30000 });
+
+	await expectScreenshot(page, testInfo, screenshotCounter, screenshotOptions);
 });
