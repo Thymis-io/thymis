20387a577497a88a03b61607e789eaa787d29c22
Eli Kogan-Wang <elikowa@gmail.com>
2025-08-18 19:06:24 +0200

    chore(deps): update @playwright/test to version 1.52.0 and npmDepsHash in default.nix

--- frontend/default.nix
+++ frontend/default.nix
@@ -9,7 +9,7 @@ buildNpmPackage {
   pname = "thymis-frontend";
   version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
   src = ./.;
-  npmDepsHash = "sha256-4YQ/cx3wuqLyq+uKzAICIUKS2cdRMfOVuDsgqXgXYOg=";
+  npmDepsHash = "sha256-djm6Tr05v6PLIBfEi3BXNQetaCqIO60j0HMbVwAXHu4=";
   dontNpmInstall = true;
   installPhase = ''
     runHook preInstall
