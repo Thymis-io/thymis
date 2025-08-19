5dbc1e9329e066e1117b02be7da051fd902a4878
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-08-19 20:54:01 +0200

    fix: login redirect

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import '$lib/i18n'; // Import to initialize. Important :)
+import { redirectToLogin } from '$lib/login';
 
-export const load = (async ({ cookies }) => {
+export const load = (async ({ fetch, cookies, url }) => {
 	const session = cookies.get('session-id');
 	if (!session) {
-		redirect(307, '/login');
+		await redirectToLogin(cookies, url, fetch);
 	} else {
 		redirect(303, '/overview');
 	}
