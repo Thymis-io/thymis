ddcb405700d738d148041388fb3017674348037f
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-10-24 19:01:29 +0200

    chore: move generate_timer_settings out

--- controller/thymis_controller/modules/bash.py
+++ controller/thymis_controller/modules/bash.py
@@ -3,7 +3,7 @@ import pathlib
 import thymis_controller.modules.modules as modules
 from thymis_controller import models
 from thymis_controller.lib import read_into_base64
-from thymis_controller.nix.templating import convert_python_value_to_nix
+from thymis_controller.nix.templating import generate_timer_settings
 from thymis_controller.project import Project
 
 
@@ -79,34 +79,6 @@ class BashModule(modules.Module):
         order=30,
     )
 
-    @staticmethod
-    def generate_timer_settings(timer_config: models.SystemdTimerType):
-        settings = []
-        if timer_config.timer_type == "realtime":
-            for calendar in timer_config.on_calendar or []:
-                if calendar:
-                    settings.append(f'timerConfig.OnCalendar = "{calendar}";')
-            if timer_config.persistent:
-                settings.append(
-                    f"timerConfig.Persistent = {convert_python_value_to_nix(timer_config.persistent)};"
-                )
-        elif timer_config.timer_type == "monotonic":
-            if timer_config.on_boot_sec:
-                settings.append(
-                    f'timerConfig.OnBootSec = "{timer_config.on_boot_sec}";'
-                )
-            if timer_config.on_unit_active_sec:
-                settings.append(
-                    f'timerConfig.OnUnitActiveSec = "{timer_config.on_unit_active_sec}";'
-                )
-        if timer_config.accuracy_sec:
-            settings.append(f'timerConfig.AccuracySec = "{timer_config.accuracy_sec}";')
-        if timer_config.randomized_delay_sec:
-            settings.append(
-                f'timerConfig.RandomizedDelaySec = "{timer_config.randomized_delay_sec}";'
-            )
-        return settings
-
     def write_nix_settings(
         self,
         f,
@@ -131,7 +103,7 @@ class BashModule(modules.Module):
         )
         if timer_config_raw is not None:
             timer_config = models.SystemdTimerType.model_validate(timer_config_raw)
-            timer_settings = self.generate_timer_settings(timer_config)
+            timer_settings = generate_timer_settings(timer_config)
         else:
             timer_settings = []
 
