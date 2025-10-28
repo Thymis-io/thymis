c0b8bcad4577d846767d64724f2aecee84a01a76
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-10-28 18:26:24 +0100

    feat: add python module

--- controller/thymis_controller/modules/builtin_modules.py
+++ controller/thymis_controller/modules/builtin_modules.py
@@ -2,6 +2,7 @@ from .bash import BashModule
 from .kiosk import Kiosk
 from .modules import Module
 from .oci_container import OCIContainers
+from .python import PythonModule
 from .thymis import ThymisDevice
 from .whatever import WhateverModule
 
@@ -11,6 +12,7 @@ ALL_MODULES: list[Module] = [
     Kiosk(),
     WhateverModule(),
     BashModule(),
+    PythonModule(),
 ]
 
 ALL_MODULES_START = ALL_MODULES.copy()
