5c7eca88322e5b1cb7b9f0e4ab1e24f6ed91ca4b
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2025-10-28 11:37:25 +0100

    fix: external repo add/delete

--- frontend/src/routes/(authenticated)/external-repositories/+page.ts
+++ frontend/src/routes/(authenticated)/external-repositories/+page.ts
@@ -1,7 +1,7 @@
 import { fetchWithNotify } from '$lib/fetchWithNotify';
 import type { PageLoad } from './$types';
 
-type ExternalRepositoryStatus = {
+export type ExternalRepositoryStatus = {
 	status: 'loading' | 'no-path' | 'no-readme' | 'no-magic-string' | 'loaded';
 	modules: string[];
 	details?: string;
@@ -10,7 +10,7 @@ type ExternalRepositoryStatus = {
 const getExternalRepositoriesStatus = async (fetch: typeof window.fetch) => {
 	const response = await fetchWithNotify('/api/external-repositories/status', undefined, {}, fetch);
 	if (response.ok) {
-		return (await response.json()) as Record<string, ExternalRepositoryStatus>;
+		return (await response.json()) as Record<string, ExternalRepositoryStatus | undefined>;
 	}
 	return {};
 };
