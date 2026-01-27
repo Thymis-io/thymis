dd1a66d552038dbe7ab92ee4afc445920d2b8d0c
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2026-01-27 13:05:47 +0100

    fix: default selected device in logs page

--- frontend/src/routes/(authenticated)/configuration/(subpages)/logs/+page.ts
+++ frontend/src/routes/(authenticated)/configuration/(subpages)/logs/+page.ts
@@ -1,3 +1,4 @@
+import { redirect } from '@sveltejs/kit';
 import { getLogs, getProgramNames } from '$lib/logs';
 import type { PageLoad } from './$types';
 
@@ -8,8 +9,16 @@ export const load = (async ({ fetch, url, parent }) => {
 	const programName = url.searchParams.get('program-name');
 	const exactProgramName = url.searchParams.get('exact-program-name') === 'true';
 
-	if (!deploymentInfos.find((info) => info.id === deploymentInfoId)) {
-		deploymentInfoId = deploymentInfos.length > 0 ? deploymentInfos[0].id : null;
+	if (!deploymentInfos.find((info) => info.id === deploymentInfoId) && deploymentInfos.length > 0) {
+		const orderedInfos = deploymentInfos.toSorted(
+			(a, b) =>
+				(b.last_seen ? new Date(b.last_seen).getTime() : -1000) -
+				(a.last_seen ? new Date(a.last_seen).getTime() : -1000)
+		);
+		deploymentInfoId = orderedInfos[0].id;
+		const searchParams = new URLSearchParams(url.searchParams);
+		searchParams.set('deployment-info-id', deploymentInfoId);
+		redirect(307, `/configuration/logs?${searchParams.toString()}`);
 	}
 
 	let logs;
