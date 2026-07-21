3342d90069c264dbc1c7320260022689845b76b5
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2026-07-21 15:03:03 +0200

    ci: pass --accept-flake-config to nix develop calls

--- .github/npm_test_integration.sh
+++ .github/npm_test_integration.sh
@@ -7,7 +7,7 @@ PYTHON=$(which python)
 OLD_PATH=$PATH
 export PATH=$OUR_NIX_PARENT
 UVICORN_PORT=$($PYTHON -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-UVICORN_PORT=$UVICORN_PORT $OUR_NIX develop .#forNpmTesting --command npm run test:integration "$@" 2>&1 | $TEE output.log
+UVICORN_PORT=$UVICORN_PORT $OUR_NIX develop --accept-flake-config .#forNpmTesting --command npm run test:integration "$@" 2>&1 | $TEE output.log
 export PATH=$OLD_PATH
 PLAYWRIGHT_EXIT_CODE=$?
 if grep -q -e "Error: A snapshot doesn't exist at" -e "Screenshot comparison failed" output.log; then
